36af3d5f2b21130d12545bd336491258c32c7cdd
ThomasCardin <tomcardin@outlook.com>
2022-08-30 13:27:36 -0400

    blockchain components

---
+++ blockchain/components/block.go
@@ -0,0 +1,56 @@
+package components
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"log"
+	"math"
+)
+
+type Block struct {
+	Index        int    `json:"Index"`
+	TimeStamp    string `json:"TimeStamp"`
+	Proof        int64  `json:"Proof"`
+	PreviousHash string `json:"PreviousHash"`
+}
+
+// Proof of work
+func (block *Block) PoW(previousProof int64) int64 {
+	var newProof int64 = 1
+	checkProof := false
+
+	for !checkProof {
+		hashOperation := sha256.New()
+		_, err := hashOperation.Write([]byte(string(rune(int64(math.Pow(float64(newProof), 2)) - int64(math.Pow(float64(previousProof), 2))))))
+		if err != nil {
+			panic(err)
+		}
+
+		hexHashSum := hex.EncodeToString(hashOperation.Sum(nil))
+		if hexHashSum[:4] == "0000" {
+			log.Println("Block mined!")
+			checkProof = true
+		} else {
+			newProof++
+		}
+	}
+
+	return newProof
+}
+
+func (block *Block) Hash() string {
+	jsonBlock, jsonErr := json.Marshal(block)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+
+	hash := sha256.New()
+	_, err := hash.Write([]byte(string(jsonBlock)))
+	if err != nil {
+		panic(err)
+	}
+
+	hexHashSum := hex.EncodeToString(hash.Sum(nil))
+	return hexHashSum
+}
---
+++ blockchain/components/chain.go
@@ -0,0 +1,80 @@
+package components
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"math"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type Chain struct {
+	Chain []Block `json:"Chain"`
+}
+
+func (chain *Chain) CreateBlock(proof int64, previousHash string) Block {
+	block := Block{
+		Index:        len(chain.Chain) + 1,
+		TimeStamp:    string(rune(time.Now().UnixNano())),
+		Proof:        proof,
+		PreviousHash: previousHash,
+	}
+
+	chain.Chain = append(chain.Chain, block)
+	return block
+}
+
+func (chain *Chain) GetPreviousBlock() Block {
+	return chain.Chain[len(chain.Chain)-1]
+}
+
+func (chain *Chain) IsChainValid() bool {
+	blockIndex := 1
+	previousBlock := chain.Chain[0]
+	for blockIndex < len(chain.Chain) {
+		block := chain.Chain[blockIndex]
+		if block.PreviousHash != previousBlock.Hash() {
+			return false
+		}
+
+		hashOperation := sha256.New()
+		_, err := hashOperation.Write([]byte(string(rune(int64(math.Pow(float64(block.Proof), 2)) - int64(math.Pow(float64(previousBlock.Proof), 2))))))
+		if err != nil {
+			panic(err)
+		}
+
+		hexHashSum := hex.EncodeToString(hashOperation.Sum(nil))
+		if hexHashSum[:4] != "0000" {
+			return false
+		}
+
+		previousBlock = block
+		blockIndex++
+	}
+
+	return true
+}
+
+func (chain *Chain) HttpMineBlock(w http.ResponseWriter, r *http.Request) {
+	previousBlock := chain.GetPreviousBlock()
+	previousProof := previousBlock.Proof
+	proof := previousBlock.PoW(previousProof)
+	previousHash := previousBlock.Hash()
+
+	block := chain.CreateBlock(proof, previousHash)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(block)
+}
+
+func (chain *Chain) HttpGetChain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(chain.Chain)
+}
+
+func (chain *Chain) HttpIsChainValid(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	w.Write([]byte(strconv.FormatBool(chain.IsChainValid())))
+}
